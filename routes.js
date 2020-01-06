779ad7c26335e62ac93140ef746deaf2b6b4c313
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-06 23:57:47 +0900

    Create routes.js
    전역적, 유저, 비디오의 경로를 변수에 저장

---
+++ routes.js
@@ -0,0 +1,42 @@
+//전역적으로 사용하는 경로 변수 선언
+const HOME = '/'
+const JOIN = '/join'
+const LOGIN = '/login'
+const LOGOUT = '/logout'
+const SEARCH = '/search'
+//
+
+// 유저
+const USERS = '/users'
+const USER_DETAIL = '/:id'
+// /users/id("hi123")
+const EDIT_PROFILE = '/edit-profile'
+const CHANGE_PASSWORD = '/change-password'
+//
+
+//비디오
+const VIDEOS = '/videos'
+const UPLOAD = '/upload'
+const VIDEOS_DETAIL = '/:id'
+const EDIT_VIDEO = '/:id/edit'
+const DELETE_VIDEO = '/:id/delete'
+
+//오브젝트 생성
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  vidoes: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEOS_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
+}
+
+export default routes
