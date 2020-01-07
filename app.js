37b48f78bc1bf7c1ba9e8c8266bf4b4065d72d82
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-07 18:14:38 +0900

    주석 추가 미들웨어 라우터 부분

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
+
+import globalRouter from './routers/globalRouter'
 import userRouter from './routers/userRouter'
 import videoRouter from './routers/videoRouter'
-import globalRouter from './routers/globalRouter'
 import routes from './routes'
 const app = express()
 
+//미들웨어
 app.use(helmet())
 app.use(morgan('dev'))
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extends: true }))
-
+//라우터
 app.use(routes.home, globalRouter)
 app.use(routes.users, userRouter)
 app.use(routes.vidoes, videoRouter)
