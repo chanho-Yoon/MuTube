65bda6345fd56dc231b4db45f653028b2f2cfcb7
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-07 18:18:54 +0900

    video,userController import
    라우팅에서 컨트롤러를 사용하여 핸들러 부분 코드 단순화

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import routes from '../routes'
+
+import { home, search } from '../controllers/videoController'
+import { join, login, logout } from '../controllers/userController'
+
 const globalRouter = express.Router()
 
-globalRouter.get(routes.home, (req, res) => res.send('hi Home!!'))
-globalRouter.get(routes.join, (req, res) => res.send('hi join!!'))
-globalRouter.get(routes.login, (req, res) => res.send('hi login!!'))
-globalRouter.get(routes.logout, (req, res) => res.send('hi logout!!'))
-globalRouter.get(routes.search, (req, res) => res.send('hi search!!'))
+globalRouter.get(routes.home, home)
+globalRouter.get(routes.search, search)
+globalRouter.get(routes.join, join)
+globalRouter.get(routes.login, login)
+globalRouter.get(routes.logout, logout)
+
 export default globalRouter
