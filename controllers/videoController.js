1490efbb4f7acf8ca589eb68e55f3de418919baf
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-14 21:23:53 +0900

    검색 템플릿에서도 video 가 보이도록 res.render에 videos 추가

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,21 +3,26 @@ import { videos } from '../js/dbtest'
 export const home = (req, res) => {
   res.render('home', { pageTitle: 'Home', testId: '1234ss', videos })
 }
+
 export const search = (req, res) => {
   const {
     query: { search_word }
   } = req
-  res.render('search', { pageTitle: 'Search', search_word })
+  res.render('search', { pageTitle: 'Search', search_word, videos })
 }
+
 export const upload = (req, res) => {
   res.render('upload', { pageTitle: 'Upload' })
 }
+
 export const videoDetail = (req, res) => {
   res.render('videoDetail', { pageTitle: 'Video Detail' })
 }
+
 export const editVideo = (req, res) => {
   res.render('editVideo', { pageTitle: 'Edit Video' })
 }
+
 export const deleteVideo = (req, res) => {
   res.render('deleteVideo', { pageTitle: 'Delete Video' })
 }
