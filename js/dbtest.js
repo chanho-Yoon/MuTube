372610b2c5f983aab7e986943d970616c00ca648
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-14 16:06:26 +0900

    mongoDB 연동 전 테스트 videos 배열 생성

---
+++ js/dbtest.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 14334,
+    title: 'video test title',
+    description: 'I love js',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1234,
+      name: 'Chanho',
+      email: 'haha@naver.com'
+    }
+  },
+  {
+    id: 43323,
+    title: 'video test title2222',
+    description: 'I love js2',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1234,
+      name: 'hohoho',
+      email: 'derwer@naver.com'
+    }
+  },
+  {
+    id: 54543,
+    title: 'video test title33333',
+    description: 'I love js3',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1234,
+      name: 'hahaha',
+      email: 'haha@naver.com'
+    }
+  },
+  {
+    id: 12874,
+    title: 'video test title44444',
+    description: 'I love js4',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1234,
+      name: 'cacaca',
+      email: 'caca@naver.com'
+    }
+  }
+]
