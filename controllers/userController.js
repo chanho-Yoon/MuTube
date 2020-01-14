72db957249d8fdf34e268ba6598a38806c83264b
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-14 21:27:12 +0900

    회원가입할 때 get 과 post 방식으로 나눔
    회원가입할 때 비밀번호 재확인 틀렸을 시 res.status(400) 반환 후 join 홈페이지 보이도록 render 반환,
    성공시 redirect(홈화면) 반환

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,25 @@
-export const join = (req, res) => res.render('Join', { pageTitle: 'Join' })
+import routes from '../routes'
+
+//GET 방식
+export const getJoin = (req, res) => {
+  res.render('Join', { pageTitle: 'Join' })
+}
+
+//POST 방식
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, ninkname, password, password2 }
+  } = req
+  if (password !== password2) {
+    //패스워드가 일치하지 않다는 상태 코드(status code) 전달
+    res.status(400)
+    res.render('Join', { pageTitle: 'Join' })
+  } else {
+    // 사용자 등록
+    // 사용자 로그인
+    res.redirect(routes.home)
+  }
+}
 export const login = (req, res) => res.render('Login', { pageTitle: 'Login' })
 export const logout = (req, res) => res.render('Logout', { pageTitle: 'Logout' })
 export const userDetail = (req, res) => res.render('UserDetail', { pageTitle: 'User Detail' })
