737f9acb10a7608773e2133ec618523f822cac03
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-15 23:05:41 +0900

    get,post login 분리

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,12 @@ export const postJoin = (req, res) => {
     res.redirect(routes.home)
   }
 }
-export const login = (req, res) => res.render('Login', { pageTitle: 'Login' })
+export const getLogin = (req, res) => {
+  res.render('Login', { pageTitle: 'Login' })
+}
+export const postLogin = (req, res) => {
+  res.redirect(routes.home)
+}
 export const logout = (req, res) => res.render('Logout', { pageTitle: 'Logout' })
 export const userDetail = (req, res) => res.render('UserDetail', { pageTitle: 'User Detail' })
 export const editProfile = (req, res) => res.render('EditProfile', { pageTitle: 'Edit Profile' })
