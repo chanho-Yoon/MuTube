df1500086ba0753e841a11330e579ccb0ae914e6
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-15 23:06:00 +0900

    DB연동 전 임시로 만든 가짜 사용자 데이터

--- js/middlewares.js
+++ js/middlewares.js
@@ -3,5 +3,11 @@ import routes from '../routes.js'
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = 'MuTube'
   res.locals.routes = routes
+
+  //임시로 만든 가짜 사용자 데이터
+  res.locals.user = {
+    idCheck: true,
+    id: 1
+  }
   next()
 }
