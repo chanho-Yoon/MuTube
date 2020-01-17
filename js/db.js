51c0771bcf938387927b1100696d142715889ef1
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-17 22:34:46 +0900

    mongoose 로 DB 연동

--- js/db.js
+++ js/db.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
+import mongoose from 'mongoose'
 
-const Cat = mongoose.model('Cat', { name: String })
+mongoose.connect('mongodb://localhost:27017/mutube', { userNewUrlParser: true, useFindAndModify: false })
 
-const kitty = new Cat({ name: 'Zildjian' })
-kitty.save().then(() => console.log('meow'))
+const db = mongoose.connection
+
+//DB Connection 성공 여부
+const handleOpen = () => console.log('Connected to DB Success')
+//DB Connection 실패시
+const handleError = error => console.log(`Error DB Connection: ${error}`)
+//한번만 실행
+db.once('open', handleOpen)
+db.on('error', handleError)
