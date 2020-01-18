1c4801d419d2c2464c77b87b6c12d4bc5f8fda36
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-18 20:37:57 +0900

    created video DB model

---
+++ model/video.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const VideoSchema = new Schema({
+  fileUrl: {
+    type: String,
+    required: 'File URL required'
+  },
+  title: {
+    type: String,
+    required: 'Title required'
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+//model 생성
+const model = mongoose.model('Video', VideoSchema)
+export default model
