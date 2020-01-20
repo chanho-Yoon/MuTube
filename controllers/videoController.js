b230a4d41113739adc1d35dabd958ab1a3e75d1d
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-21 01:15:38 +0900

    upload 구현 및 주석으로 컨트롤러 가독성 향상시킴
    upload post로 받아온 것을 body에서는 title, description를 가져오고, file 에선 동영상 fileURL을 가져오기 위해서 path 가져옴,  주석으로 수평성을 그어 가독성 향상시킴

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,33 +10,44 @@ export const home = async (req, res) => {
     res.render('home', { pageTitle: 'Home', videos })
   }
 }
-
+//검색 --------------------------------------------------------------------------------------------------
 export const search = (req, res) => {
   const {
     query: { search_word }
   } = req
   res.render('search', { pageTitle: 'Search', search_word, videos })
 }
-
+//업로드 --------------------------------------------------------------------------------------------------
 export const getUpload = (req, res) => {
   res.render('upload', { pageTitle: 'Upload' })
 }
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description }
+    body: { title, description },
+    file: { path }
   } = req
+  console.log(`path: ${path}`)
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  })
+  console.log(newVideo)
   //비디오 업로드 및 저장
-  res.redirect(routes.videoDetail(12345))
+  res.redirect(routes.videoDetail(newVideo.id))
 }
 
+//비디오 상세 --------------------------------------------------------------------------------------------------
 export const videoDetail = (req, res) => {
   res.render('videoDetail', { pageTitle: 'Video Detail' })
 }
 
+//비디오 수정 --------------------------------------------------------------------------------------------------
 export const editVideo = (req, res) => {
   res.render('editVideo', { pageTitle: 'Edit Video' })
 }
 
+//비디오 삭제 --------------------------------------------------------------------------------------------------
 export const deleteVideo = (req, res) => {
   res.render('deleteVideo', { pageTitle: 'Delete Video' })
 }
