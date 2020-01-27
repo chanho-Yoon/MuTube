bb4c4599d865fe1a489bed48526a44f670223644
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-27 22:26:40 +0900

    라우터에 주석 추가 및 editVideo get,post 분리
    비디오 수정 클릭시 get방식으로 해당 id를 찾아 비디오를 보여주고, post 에서는 수정한 내용을 받음

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import routes from '../routes'
-import { videoDetail, editVideo, deleteVideo, getUpload, postUpload } from '../controllers/videoController'
+import { videoDetail, getEditVideo, deleteVideo, getUpload, postUpload, postEditVideo } from '../controllers/videoController'
 import { uploadVideo } from '../js/middlewares'
 
 const videoRouter = express.Router()
 
-//업로드
+// 업로드
 videoRouter.get(routes.upload, getUpload)
 videoRouter.post(routes.upload, uploadVideo, postUpload)
-
+// 비디오 상세
 videoRouter.get(routes.videoDetail(), videoDetail)
-videoRouter.get(routes.editVideo, editVideo)
+// 비디오 수정
+videoRouter.get(routes.editVideo(), getEditVideo)
+videoRouter.post(routes.editVideo(), postEditVideo)
+// 비디오 삭제
 videoRouter.get(routes.deleteVideo, deleteVideo)
 
 export default videoRouter
