25960dbbc5f471760e36d32a36b575413304afb9
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-27 22:27:57 +0900

    get,post EditVideo 작성
    getEditVideo : 받은 id로 해당하는 비디오를 찾아 video를 보여주고,
    postEditVideo : 수정한 내용을 받아 findOneAndUpdate로 해당하는 _id 값을 받고 title과 description을 수정함

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -54,10 +54,29 @@ export const videoDetail = async (req, res) => {
 }
 
 //비디오 수정 --------------------------------------------------------------------------------------------------
-export const editVideo = (req, res) => {
-  res.render('editVideo', { pageTitle: 'Edit Video' })
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req
+  try {
+    const video = await Video.findById(id)
+    res.render('editVideo', { pageTitle: `Edit ${video.title}`, video })
+  } catch (error) {
+    res.redirect(routes.home)
+  }
 }
-
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description })
+    res.redirect(routes.videoDetail(id))
+  } catch (error) {
+    res.redirect(routes.home)
+  }
+}
 //비디오 삭제 --------------------------------------------------------------------------------------------------
 export const deleteVideo = (req, res) => {
   res.render('deleteVideo', { pageTitle: 'Delete Video' })
