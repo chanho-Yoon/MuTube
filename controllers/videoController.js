e5e0582309017f1120334c59f5a1baba216aea1e
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-30 23:49:36 +0900

    pageTitle: video.title 로 수정 / videoController 비디오 삭제 기능 추가
    비디오 상세로 접속했을시에 해당 비디오 title을 보여주도록 하기 위해 video.title로 수정
    
    비디오 수정 화면에서 삭제 버튼 눌렀을 시에 비디오가 삭제되는 기능 작성

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -34,7 +34,7 @@ export const postUpload = async (req, res) => {
     description
   })
   console.log(newVideo)
-  //비디오 업로드 및 저장
+
   res.redirect(routes.videoDetail(newVideo.id))
 }
 
@@ -46,7 +46,7 @@ export const videoDetail = async (req, res) => {
   try {
     const video = await Video.findById(id)
     console.log(video)
-    res.render('videoDetail', { pageTitle: 'Video Detail', video })
+    res.render('videoDetail', { pageTitle: video.title, video })
   } catch (error) {
     //에러 발생시 홈으로 강제이동
     res.redirect(routes.home)
@@ -78,6 +78,14 @@ export const postEditVideo = async (req, res) => {
   }
 }
 //비디오 삭제 --------------------------------------------------------------------------------------------------
-export const deleteVideo = (req, res) => {
-  res.render('deleteVideo', { pageTitle: 'Delete Video' })
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req
+  try {
+    await Video.findOneAndRemove({ _id: id })
+  } catch (error) {
+    console.log(`Controller deleteVideo error : ${error}`)
+  }
+  res.redirect(routes.home)
 }
