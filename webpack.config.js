93e8d494d5b9ecfd480407f77a7e6de62dd78672
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-01 23:31:30 +0900

    postcss-loader plugin으로 autoprefixer 사용
    옵션으로는 browsers: 'cover 99.5%'

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 const path = require('path')
+const autoprefixer = require('autoprefixer')
 const ExtractCSS = require('extract-text-webpack-plugin')
 
 const MODE = process.env.WEBPACK_ENV
@@ -13,13 +14,20 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(.scss)$/,
+        test: /\.(scss)$/,
         use: ExtractCSS.extract([
+          //로더 순서 기본적으로 오른쪽에서 왼쪽 or 아래에서부터 위
           {
             loader: 'css-loader'
           },
           {
-            loader: 'postcss-loader'
+            loader: 'postcss-loader',
+            options: {
+              plugin() {
+                //여러 개의 plugin들을 추가할 수 있음
+                return [autoprefixer({ browsers: 'cover 99.5%' })]
+              }
+            }
           },
           {
             loader: 'sass-loader'
@@ -30,8 +38,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: '[name].[format]'
-  }
+    filename: '[name].js'
+  },
+  plugins: [new ExtractCSS('styles.css')]
 }
 
 module.exports = config
