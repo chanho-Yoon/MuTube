0b621a9bd173a54b1b20d9fe30beffa81856e39b
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-03 23:31:24 +0900

    babel-polyfill 인스톨 및 webpack.config.js -> entry 추가

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,19 @@ const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js', 'main.js')
 const OUTPUT_DIR = path.join(__dirname, 'static')
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ['@babel/polyfill', ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+
       {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
@@ -23,7 +32,7 @@ const config = {
           {
             loader: 'postcss-loader',
             options: {
-              plugin() {
+              plugins() {
                 //여러 개의 plugin들을 추가할 수 있음
                 return [autoprefixer({ browsers: 'cover 99.5%' })]
               }
