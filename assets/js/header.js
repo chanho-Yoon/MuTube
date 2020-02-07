89304f378cda58251460311efca439cf6a80584d
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-08 01:45:32 +0900

    searchClick event 처리 함수 , 화면 변화 감지, 그에따른 css 처리

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,34 @@
 import '../scss/styles.scss'
 
+//검색 클릭 이벤트 처리
+const search__click = document.getElementById('search__click')
+
+function searchClick(event) {
+  alert('search 클릭했네요?')
+  console.log(event)
+}
+
+search__click.addEventListener('click', searchClick)
+//-----------------------------------------------------------------------------
+//윈도우 창 화면 변화 감지 , 그에 따른 css 처리
+function loadWindowWidth() {
+  const headerHidden = document.getElementById('header__search')
+  const header_ul_list = document.getElementById('ul-list')
+  let width = innerWidth
+
+  if (width < 1024) {
+    headerHidden.classList.add('header__column-noshow')
+    header_ul_list.classList.remove('ul-list')
+  }
+  if (width > 1024) {
+    headerHidden.classList.remove('header__column-noshow')
+    header_ul_list.classList.add('ul-list')
+  }
+}
+
 document.getElementsByTagName('BODY')[0].onresize = function() {
   loadWindowWidth()
 }
 
-function loadWindowWidth() {
-  let width = innerWidth
-
-  if (width < 1024) {
-    console.log(width)
-  }
-}
-
 addEventListener('onresize', loadWindowWidth)
+//-----------------------------------------------------------------------------
