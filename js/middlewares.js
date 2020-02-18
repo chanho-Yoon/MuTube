1dbc6968b56e2812f18e6e61b59222a7cbdb02d2
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-18 20:21:22 +0900

    로그인 중이라면 passport로 로그인처리 됐을시 저장된 user 정보를 받아옴

--- js/middlewares.js
+++ js/middlewares.js
@@ -7,11 +7,8 @@ export const localMiddleware = (req, res, next) => {
   res.locals.siteName = 'MuTube'
   res.locals.routes = routes
 
-  //임시로 만든 가짜 사용자 데이터
-  res.locals.user = {
-    idCheck: true,
-    id: 1
-  }
+  //사용자 user
+  res.locals.user = req.user || {}
   next()
 }
 
