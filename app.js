ba81dd72322dd977b08b4a0cf0481c185b2fdb0f
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-18 23:50:39 +0900

    app.use(session({}) 추가 option은 secret, resave, saveUninitialized 추가

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
+/* eslint-disable no-undef */
 import express from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import passport from 'passport'
-import './passport'
 
+import session from 'express-session'
+import { localMiddleware } from './js/middlewares'
 import globalRouter from './routers/globalRouter'
 import userRouter from './routers/userRouter'
 import videoRouter from './routers/videoRouter'
 import routes from './routes'
-import { localMiddleware } from './js/middlewares'
+
+import './passport'
 
 const app = express()
 
+app.use(helmet())
 //Pug view engine
 app.set('view engine', 'pug')
 //미들웨어
 app.use('/uploads', express.static('uploads'))
 app.use('/static', express.static('static'))
 
-app.use(helmet())
-app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extends: true }))
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false
+  })
+)
 app.use(passport.initialize())
 app.use(passport.session())
 
