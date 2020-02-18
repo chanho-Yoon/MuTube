cbb45992982486a5502997e860c2ac653e74bafc
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-18 20:18:41 +0900

    import passport / app.use passport 추가
    app.use(passport.initalize())
    app.use(passport.session())
    추가

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
+import passport from 'passport'
+import './passport'
 
 import globalRouter from './routers/globalRouter'
 import userRouter from './routers/userRouter'
 import videoRouter from './routers/videoRouter'
 import routes from './routes'
 import { localMiddleware } from './js/middlewares'
+
 const app = express()
 
 //Pug view engine
@@ -19,10 +22,13 @@ app.use('/static', express.static('static'))
 
 app.use(helmet())
 app.use(morgan('dev'))
-
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extends: true }))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 //localMiddleware
 app.use(localMiddleware)
 
