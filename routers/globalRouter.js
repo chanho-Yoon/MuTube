0f6a030c0962e6855c214f9893da16c375de25c9
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-20 20:33:18 +0900

    github로 로그인기능 구현 / logout 부분 onlyPrivate로 미들웨어 변경

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,9 +1,9 @@
 import express from 'express'
+import passport from 'passport'
 import routes from '../routes'
-
 import { home, search } from '../controllers/videoController'
-import { logout, getJoin, postJoin, postLogin, getLogin } from '../controllers/userController'
-import { onlyPublic } from '../js/middlewares'
+import { logout, getJoin, postJoin, postLogin, getLogin, githubLogin, postGithubLogIn } from '../controllers/userController'
+import { onlyPublic, onlyPrivate } from '../js/middlewares'
 
 const globalRouter = express.Router()
 
@@ -15,6 +15,11 @@ globalRouter.post(routes.login, onlyPublic, postLogin)
 
 globalRouter.get(routes.home, home)
 globalRouter.get(routes.search, search)
-globalRouter.get(routes.logout, onlyPublic, logout)
+globalRouter.get(routes.logout, onlyPrivate, logout)
+
+globalRouter.get(routes.github, githubLogin)
+globalRouter.get(routes.githubCallback, passport.authenticate('github', { failureRedirect: '/login' }), postGithubLogIn)
+// globalRouter.get(routes.faceBook, facebookLogin)
+// globalRouter.get(routes.gitHub, googleLogin)
 
 export default globalRouter
