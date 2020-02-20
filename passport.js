6ed981cdbae187bde0f68fac1f2c8bb39a2620fa
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-20 18:22:10 +0900

    passport.use에 깃허브로 가입 로그인 기능 추가

--- passport.js
+++ passport.js
@@ -1,7 +1,18 @@
+/* eslint-disable no-undef */
 import passport from 'passport'
 import User from './models/User'
-
+import GithubStrategy from 'passport-github'
+import { githubLoginCallback } from './controllers/userController'
 passport.use(User.createStrategy())
+
+passport.use(
+  new GithubStrategy({
+    clientID: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+    callbackURL: 'http://localhost:4000/auth/github/callback'
+  }),
+  githubLoginCallback
+)
 
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
