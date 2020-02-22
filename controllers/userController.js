598173948d93f5fbc7f5394ecc7f2107940e3aa8
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-22 20:31:20 +0900

    깃허브 로그인 인증 구현
    깃허브로 로그인했을 때 사용자가 있다면 바로 로그인처리하고, 없을 경우 신규 사용자를 등록하도록함

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,11 +38,32 @@ export const getLogin = (req, res) => {
 //github로 로그인
 export const githubLogin = passport.authenticate('github')
 
-export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
-  console.log(accessToken, refreshToken, profile, cb)
+export const githubLoginCallback = async (accessToken, refreshToken, profile, cb) => {
+  const {
+    _json: { id, avatar_url, name, email }
+  } = profile
+  try {
+    const user = await User.findOne({ email: email })
+    //동일한 이메을 가졌을 때는 이미 가입중인 사용자라 바로 로그인하도록 아니라면 신규 사용자 생성
+    if (user) {
+      user.githubId = id
+      user.save()
+      return cb(null, user)
+    } else {
+      const newUser = await User.create({
+        email,
+        name,
+        githubId: id,
+        avatarUrl: avatar_url
+      })
+      return cb(null, newUser)
+    }
+  } catch (error) {
+    return cb(error)
+  }
 }
 export const postGithubLogIn = (req, res) => {
-  res.send(routes.home)
+  res.redirect(routes.home)
 }
 ///////////////////////////////
 
