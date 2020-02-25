020bf5df49a7a40bb1d2a6ac1bde213011c0e004
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-26 02:21:49 +0900

    import passport-google-oauth20 및 passport.use 작성

--- passport.js
+++ passport.js
@@ -3,8 +3,8 @@ import passport from 'passport'
 import User from './models/User'
 import GithubStrategy from 'passport-github'
 import FacebookStrategy from 'passport-facebook'
-
-import { githubLoginCallback, facebookLoginCallback } from './controllers/userController'
+import GoogleStrategy from 'passport-google-oauth20'
+import { githubLoginCallback, facebookLoginCallback, googleLoginCallback } from './controllers/userController'
 import routes from './routes'
 passport.use(User.createStrategy())
 
@@ -28,6 +28,18 @@ passport.use(
       callbackURL: `https://903309e6.ngrok.io${routes.facebookCallback}`
     },
     facebookLoginCallback
+  )
+)
+
+//google Authenticate
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: `http://localhost:4000${routes.googleCallback}`
+    },
+    googleLoginCallback
   )
 )
 
