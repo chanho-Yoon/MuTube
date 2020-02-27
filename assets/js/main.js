63200fd28e8c9fe6de88a4b344d84e7750fcfd8c
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-02-27 20:45:23 +0900

    socialLogin.pug로 이동하기 전에 getEleById를 못찾아 에러출력으로 삭제
    socialLogin.pug에 직접 sciprt써서 작성

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,1 @@
 import '../scss/styles.scss'
-
-const facebookButton = document.getElementById('social-login__facebook')
-
-function readyServices() {
-  alert('서비스 준비 중 입니다. 깃허브 or 구글 인증을 사용해주세요.')
-}
-
-facebookButton.addEventListener('click', readyServices)
