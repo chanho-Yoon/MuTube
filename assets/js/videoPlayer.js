933476b46b2f11a206a511b9b9b308f1fc721e18
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-08 00:18:24 +0900

    play, volume 클릭시 해당이벤트 구현 / 특수키로 재생,정지 기능 구현 (fn키는 고민을..)

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,21 +1,45 @@
 const videoContainer = document.getElementById('jsVideoPlayer')
 const videoPlayer = document.querySelector('#jsVideoPlayer video')
 const playBtn = document.getElementById('jsPlayButton')
+const volumeBtn = document.getElementById('jsVolumeButton')
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
-    console.log(playBtn)
     videoPlayer.play()
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>'
   } else {
-    console.log(playBtn)
-
     videoPlayer.pause()
+    playBtn.innerHTML = '<i class="fas fa-play"></i>'
+  }
+}
+
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>'
+  } else {
+    videoPlayer.muted = true
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>'
   }
 }
 function init() {
   playBtn.addEventListener('click', handlePlayClick)
+  volumeBtn.addEventListener('click', handleVolumeClick)
 }
 
 if (videoContainer) {
   init()
+}
+
+// function key 는 받는게 없는데 어떻게 키보드에서 펑션키로 재생 눌렀을때의 값을 받지..
+window.onkeydown = event => {
+  if (event.keyCode == 119) {
+    if (videoPlayer.paused) {
+      videoPlayer.play()
+      playBtn.innerHTML = '<i class="fas fa-pause"></i>'
+    } else {
+      videoPlayer.pause()
+      playBtn.innerHTML = '<i class="fas fa-play"></i>'
+    }
+  }
 }
