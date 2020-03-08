320fa06f5c156524800b66fd880e2d784eef4baa
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-09 03:32:01 +0900

    비디오 컨트롤러에 시간표시 ( duration, currentTime)

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,8 +1,11 @@
 const videoContainer = document.getElementById('jsVideoPlayer')
-const videoPlayer = document.querySelector('#jsVideoPlayer video')
+const videoPlayer = document.querySelector('#jsVideoPlayer #videoId')
 const playBtn = document.getElementById('jsPlayButton')
 const volumeBtn = document.getElementById('jsVolumeButton')
 const screenBtn = document.getElementById('jsScreenButton')
+const currentTime = document.getElementById('currentTime')
+const totalTime = document.getElementById('totalTime')
+const videoId = document.getElementById('videoId')
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -27,21 +30,70 @@ function handleVolumeClick() {
 function exitScreenClick() {
   screenBtn.innerHTML = '<i class="fas fa-expand"></i>'
   screenBtn.addEventListener('click', fullScreenClick)
-  document.webkitExitFullscreen()
+  if (document.exitFullscreen) {
+    document.exitFullscreen()
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen()
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen()
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen()
+  }
 }
 
 function fullScreenClick() {
-  videoContainer.webkitRequestFullscreen()
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen()
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen()
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen()
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen()
+  }
   screenBtn.innerHTML = '<i class="fas fa-compress"></i>'
   screenBtn.removeEventListener('click', fullScreenClick)
   screenBtn.addEventListener('click', exitScreenClick)
 }
 
+function formatDate(seconds) {
+  const secondsNumber = parseInt(seconds, 10)
+  let hours = Math.floor(secondsNumber / 3600)
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60)
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60
+
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`
+  }
+  return `${hours}:${minutes}:${totalSeconds}`
+}
+
+//잘 작동할때가 있고 안될때가 있고 고민을 해봐야곘네,.,
+function setTotalTime() {
+  console.log('실행됨')
+  setInterval(getCurrentTime, 1000)
+  totalTime.innerHTML = formatDate(videoPlayer.duration)
+}
+
+function getCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime)
+}
+
 function init() {
+  //비디오가 로드 되기 전에 시간을 호출하면 Nan 호출되는걸 방지
   playBtn.addEventListener('click', handlePlayClick)
   volumeBtn.addEventListener('click', handleVolumeClick)
   screenBtn.addEventListener('click', fullScreenClick)
+  videoId.addEventListener('loadedmetadata', setTotalTime)
 }
+
+// eslint-disable-next-line no-constant-condition
 
 if (videoContainer) {
   init()
