f498fccde0ffe4dc55d3acb94b7d2c8b4541fc2c
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-12 00:04:26 +0900

    녹화 시작, 중지, 다운로드 기능 작성

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,9 +2,35 @@ const recorderContainer = document.getElementById('jsRecordContainer')
 const recordBtn = document.getElementById('jsRecordBtn')
 const videoPreview = document.getElementById('jsVideoPreview')
 
-const startRecording = async () => {
+let streamObject
+let videoRecorder
+
+// video 다운로드
+const handleVideoData = event => {
+  const { data: videoFile } = event
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(videoFile)
+  link.download = 'recorded.webm'
+  document.body.appendChild(link)
+  link.click()
+}
+// 녹화 중지
+const stopRecording = () => {
+  videoRecorder.stop()
+  recordBtn.removeEventListener('click', stopRecording)
+  recordBtn.addEventListener('click', getVideo)
+  recordBtn.innerHTML = '녹화'
+}
+// 녹화 시작
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject)
+  videoRecorder.start()
+  videoRecorder.addEventListener('dataavailable', handleVideoData)
+  recordBtn.addEventListener('click', stopRecording)
+}
+// 카메라에서 비디오 가져오기
+const getVideo = async () => {
   try {
-    //media 접근 기다림
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: { width: 1280, height: 720 }
@@ -12,14 +38,18 @@ const startRecording = async () => {
     videoPreview.srcObject = stream
     videoPreview.muted = true
     videoPreview.play()
+    recordBtn.innerHTML = '녹화 중지'
+    streamObject = stream
+    startRecording()
   } catch (error) {
-    recordBtn.innerHTML = '녹화 불가능😂'
-    recordBtn.removeEventListener('click', startRecording)
+    recordBtn.innerHTML = '녹화 불가능'
+  } finally {
+    recordBtn.removeEventListener('click', getVideo)
   }
 }
 
 function init() {
-  recordBtn.addEventListener('click', startRecording)
+  recordBtn.addEventListener('click', getVideo)
 }
 
 if (recorderContainer) {
