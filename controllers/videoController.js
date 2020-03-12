6c4ee3e2fd9689e0e6ec8208de98c6bb81f7d132
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-12 23:59:05 +0900

    비디오 컨트롤러 -> 댓글 추가 함수 작성

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from '../routes'
 import Video from '../models/Video'
+import Comment from '../models/Comment'
 
 export const home = async (req, res) => {
   try {
@@ -53,7 +54,9 @@ export const videoDetail = async (req, res) => {
   } = req
   try {
     // populate() 사용하여 id에 해당하는 비디오를 찾아 creator 객체도 같이 넘겨준다
-    const video = await Video.findById(id).populate('creator')
+    const video = await Video.findById(id)
+      .populate('creator')
+      .populate('comments')
     res.render('videoDetail', { pageTitle: video.title, video })
   } catch (error) {
     //에러 발생시 홈으로 강제이동
@@ -119,6 +122,29 @@ export const postRegisterView = async (req, res) => {
     video.views = video.views + 1
     video.save()
     res.status(200)
+  } catch (error) {
+    res.status(400)
+  } finally {
+    res.end()
+  }
+}
+
+// add comment
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user
+  } = req
+  try {
+    const video = await Video.findById(id)
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id,
+      avatarUrl: user.id.avatarUrl
+    })
+    video.comments.push(newComment.id)
+    video.save()
   } catch (error) {
     res.status(400)
   } finally {
