72d294013d781f53fd91b0ad06b54231f00c5b43
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-18 21:39:45 +0900

    AWS S3에 비디오와 이미지 파일 업로드 및 비디오 삭제 추가 작성

--- js/middlewares.js
+++ js/middlewares.js
@@ -1,8 +1,59 @@
+/* eslint-disable no-undef */ //process에 빨간줄 해결
 import multer from 'multer'
-import routes from '../routes.js'
+import multerS3 from 'multer-s3'
+import aws from 'aws-sdk'
+import routes from '../routes'
+import Video from '../models/Video'
 
-const multerVideo = multer({ dest: 'uploads/videos/' })
-const multerAvatar = multer({ dest: 'uploads/avatars/' })
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: 'ap-northeast-2'
+})
+// upload video
+const multerVideo = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: 'public-read',
+    bucket: 'mutube/video'
+  })
+})
+// upload avatar
+const multerAvatar = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: 'public-read',
+    bucket: 'mutube/avatar'
+  })
+})
+
+export const uploadVideo = multerVideo.single('videoFile')
+export const uploadAvatar = multerAvatar.single('avatar')
+
+// delete video
+export const awsDeleteVideo = async (req, res, next) => {
+  const {
+    params: { id }
+  } = req
+  const video = await Video.findById(id)
+  const url = video.fileUrl.split('/')
+  const delFileName = url[url.length - 1]
+  const params = {
+    Bucket: 'mutube/video',
+    Key: delFileName
+  }
+  s3.deleteObject(params, function(err, data) {
+    if (err) {
+      console.log('aws video delete error')
+      console.log(err, err.stack)
+      res.redirect(routes.home)
+    } else {
+      console.log('aws video delete success' + data)
+    }
+  })
+  next()
+}
+
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = 'MuTube'
   res.locals.routes = routes
@@ -27,7 +78,3 @@ export const onlyPrivate = (req, res, next) => {
     res.redirect(routes.home)
   }
 }
-
-export const uploadVideo = multerVideo.single('videoFile')
-
-export const uploadAvatar = multerAvatar.single('avatar')
