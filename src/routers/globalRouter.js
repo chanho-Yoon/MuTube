8f7d4b86d5dfe1d0b6d037ee6a996b27258cc6aa
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-20 23:01:35 +0900

    소셜 로그인 시 이벤트 메시지 출력을 위해 succesFlash, failureFlash 추가 작성

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -31,14 +31,38 @@ globalRouter.get(routes.search, search)
 globalRouter.get(routes.logout, onlyPrivate, logout)
 
 globalRouter.get(routes.github, githubLogin)
-globalRouter.get(routes.githubCallback, passport.authenticate('github', { failureRedirect: '/login' }), postGithubLogIn)
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate('github', {
+    failureRedirect: '/login',
+    successFlash: '로그인 성공',
+    failureFlash: '로그인 실패, 아이디 또는 비밀번호를 확인해주세요'
+  }),
+  postGithubLogIn
+)
 
 //페이스북 https로 인해 값 받아오는 것까지 완료, 터널링 ngrok 해봤지만 페이스북 자체에서 부정이라 등록이 안된다고함..
 globalRouter.get(routes.facebook, facebookLogin)
-globalRouter.get(routes.facebookCallback, passport.authenticate('facebook', { failureRedirect: '/login' }), postFacebookLogIn)
+globalRouter.get(
+  routes.facebookCallback,
+  passport.authenticate('facebook', {
+    failureRedirect: '/login',
+    successFlash: '로그인 성공',
+    failureFlash: '로그인 실패, 아이디 또는 비밀번호를 확인해주세요'
+  }),
+  postFacebookLogIn
+)
 
 globalRouter.get(routes.google, googleLogin)
-globalRouter.get(routes.googleCallback, passport.authenticate('google', { failureRedirect: '/login' }), postGoogleLogin)
+globalRouter.get(
+  routes.googleCallback,
+  passport.authenticate('google', {
+    failureRedirect: '/login',
+    successFlash: '로그인 성공',
+    failureFlash: '로그인 실패, 아이디 또는 비밀번호를 확인해주세요'
+  }),
+  postGoogleLogin
+)
 
 globalRouter.get(routes.me, getMe)
 
